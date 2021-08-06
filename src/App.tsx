efe89b0d7a184dd1c17007335253098514237efe
Fabio Ramos <fabioalvesramos12@gmail.com>
2021-08-06 19:46:46 -0300

    added context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { WeatherContextProvider } from "./context/WeatherContext";
 import { Home } from "./pages/Home";
 
 import GlobalStyles from "./styles/global";
 
 function App() {
     return (
-        <>
+        <WeatherContextProvider>
             <Home />
             <GlobalStyles />
-        </>
+        </WeatherContextProvider>
     );
 }
 
