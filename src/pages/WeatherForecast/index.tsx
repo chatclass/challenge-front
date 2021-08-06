982c9b282f01cae8d2e4d551f3ee162008a20aa6
Fabio Ramos <fabioalvesramos12@gmail.com>
2021-08-06 19:49:09 -0300

    updating weatherforecast with weatherdata

--- src/pages/WeatherForecast/index.tsx
+++ src/pages/WeatherForecast/index.tsx
@@ -1,11 +1,100 @@
-import { WeatherForecastContainer, SearchCities } from "./styles";
+import {
+    WeatherForecastContainer,
+    SearchCities,
+    CitySearched,
+    ClimeInfos,
+} from "./styles";
 
 import searchImg from "../../assets/search-icon.svg";
+import arrowUpImg from "../../assets/arrow-up-icon.svg";
+import arrowDownImg from "../../assets/arrow-down-icon.svg";
+
+import { useContext } from "react";
+import { WeatherContext } from "../../context/WeatherContext";
 
 export function WeatherForecast() {
+    const { weatherData, getData, getCityName } = useContext(WeatherContext);
+
+    function capitalize(str: string) {
+        return str.charAt(0).toUpperCase() + str.substr(1);
+    }
+
+    function formatSpeed(speed: number) {
+        return speed * 3.6;
+    }
+
     return (
         <WeatherForecastContainer>
             <h1>Previsão do tempo</h1>
+
+            {weatherData !== null ? (
+                <CitySearched>
+                    <div className="day-state">
+                        <span className="location">
+                            {weatherData.name} - {weatherData.sys.country}
+                        </span>
+
+                        <div className="clime-container">
+                            <span className="temperature">
+                                {Math.round(weatherData.main.temp)}&deg;C
+                            </span>
+
+                            <span className="climate">
+                                {capitalize(weatherData.weather[0].description)}
+                            </span>
+                        </div>
+
+                        <ClimeInfos>
+                            <div className="min-max-temperature">
+                                <span className="min">
+                                    <img
+                                        src={arrowDownImg}
+                                        alt="Temperatura mínima"
+                                    />
+                                    <strong>
+                                        {Math.round(weatherData.main.temp_min)}
+                                        &deg;
+                                    </strong>
+                                </span>
+
+                                <span className="max">
+                                    <img
+                                        src={arrowUpImg}
+                                        alt="Temperatura máxima"
+                                    />
+                                    <strong>
+                                        {Math.round(weatherData.main.temp_max)}
+                                        &deg;
+                                    </strong>
+                                </span>
+                            </div>
+
+                            <span className="feels-like">
+                                Sensação
+                                <strong>
+                                    {Math.round(weatherData.main.feels_like)}
+                                    &deg;C
+                                </strong>
+                            </span>
+
+                            <span className="wind">
+                                Vento
+                                <strong>
+                                    {Math.round(
+                                        formatSpeed(weatherData.wind.speed)
+                                    )}
+                                    km/h
+                                </strong>
+                            </span>
+
+                            <span className="humidity">
+                                Umidade
+                                <strong>{weatherData.main.humidity}%</strong>
+                            </span>
+                        </ClimeInfos>
+                    </div>
+                </CitySearched>
+            ) : null}
 
             <SearchCities>
                 <input
@@ -13,8 +102,9 @@ export function WeatherForecast() {
                     id="city-search"
                     name="search"
                     placeholder="Insira aqui o nome da cidade"
+                    onChange={(input) => getCityName(input.target.value)}
                 />
-                <button>
+                <button onClick={() => getData()}>
                     <img src={searchImg} alt="Buscar nome da cidade" />
                 </button>
             </SearchCities>
--- src/pages/WeatherForecast/styles.ts
+++ src/pages/WeatherForecast/styles.ts
@@ -6,7 +6,7 @@ export const WeatherForecastContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 6.4rem;
+    gap: 3.2rem;
 
     padding: 6.4rem 0;
     border-bottom: 1px solid #fff;
@@ -16,6 +16,91 @@ export const WeatherForecastContainer = styled.div`
         color: #fff;
     }
 `;
+
+export const CitySearched = styled.div`
+    width: 100%;
+
+    background: #fff;
+    padding: 2.4rem;
+
+    .day-state {
+        padding: 0 2.4rem;
+        border-bottom: 1px solid #ff7f00;
+    }
+
+    .location {
+        font-size: 1.6rem;
+        font-weight: 600;
+        color: #505050;
+    }
+
+    .clime-container {
+        display: flex;
+        gap: 2.4rem;
+
+        font-size: 3.6rem;
+        font-weight: 600;
+        color: #505050;
+
+        margin-top: 1.6rem;
+    }
+`;
+
+export const ClimeInfos = styled.div`
+    width: fit-content;
+    padding: 2.4rem 0;
+
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 1.6rem 3.2rem;
+
+    .min-max-temperature {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    span {
+        font-size: 1.6rem;
+    }
+
+    .min {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .max {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .min img {
+        color: #ff7f00;
+        height: 18px;
+        width: 25px;
+    }
+
+    .max img {
+        color: #ff7f00;
+        height: 18px;
+        width: 25px;
+    }
+
+    .feels-like strong {
+        margin-left: 0.8rem;
+    }
+
+    .wind strong {
+        margin-left: 0.8rem;
+    }
+
+    .humidity strong {
+        margin-left: 0.8rem;
+    }
+`;
+
 export const SearchCities = styled.div`
     width: 80%;
     display: flex;
