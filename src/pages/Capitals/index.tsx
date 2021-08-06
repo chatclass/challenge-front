febd59eab9b4c9c8e041560e93303c5da4da52f4
Fabio Ramos <fabioalvesramos12@gmail.com>
2021-08-06 19:47:53 -0300

    created capitals component

--- src/pages/Capitals/index.tsx
+++ src/pages/Capitals/index.tsx
@@ -1,10 +1,37 @@
+import { useContext } from "react";
+import { WeatherContext } from "../../context/WeatherContext";
 import { CapitalsWeather, CapitalsContainer } from "./styles";
 
 export function Capitals() {
+    const { capitalsData } = useContext(WeatherContext);
+    console.log(capitalsData);
+
     return (
         <CapitalsContainer>
             <h2>Capitais</h2>
-            <CapitalsWeather></CapitalsWeather>
+            <CapitalsWeather>
+                <div className="min-max-labels">
+                    <span>Min</span>
+                    <span>Max</span>
+                </div>
+                {capitalsData.lenght !== 0
+                    ? capitalsData.map((capital: any, index: string) => {
+                          return (
+                              <div key={index} className="cities">
+                                  <span>
+                                      {Math.round(capital.main.temp_min)}
+                                      &deg;
+                                  </span>
+                                  <span>
+                                      {Math.round(capital.main.temp_max)}
+                                      &deg;
+                                  </span>
+                                  <span>{capital.name}</span>
+                              </div>
+                          );
+                      })
+                    : null}
+            </CapitalsWeather>
         </CapitalsContainer>
     );
 }
--- src/pages/Capitals/styles.ts
+++ src/pages/Capitals/styles.ts
@@ -10,4 +10,27 @@ export const CapitalsContainer = styled.div`
         color: #fff;
     }
 `;
-export const CapitalsWeather = styled.div``;
+export const CapitalsWeather = styled.div`
+    .min-max-labels {
+        display: flex;
+        align-items: center;
+        gap: 1.6rem;
+
+        margin-top: 2.4rem;
+        margin-bottom: 1.6rem;
+
+        font-size: 1.6;
+        color: #505050;
+    }
+
+    .cities {
+        display: flex;
+        align-items: center;
+        gap: 1.6rem;
+
+        margin-bottom: 1.6rem;
+
+        font-size: 1.6;
+        font-weight: 600;
+    }
+`;
