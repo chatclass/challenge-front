6f7f0d65562594cae9e24f237273d663cf232940
Fabio Ramos <fabioalvesramos12@gmail.com>
2021-08-06 13:39:44 -0300

    Created capitals layout

---
+++ src/pages/Capitals/index.tsx
@@ -0,0 +1,10 @@
+import { CapitalsWeather, CapitalsContainer } from "./styles";
+
+export function Capitals() {
+    return (
+        <CapitalsContainer>
+            <h2>Capitais</h2>
+            <CapitalsWeather></CapitalsWeather>
+        </CapitalsContainer>
+    );
+}
---
+++ src/pages/Capitals/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CapitalsContainer = styled.div`
+    width: 80%;
+
+    padding: 2.4rem 0;
+
+    h2 {
+        font-size: 3.2rem;
+        color: #fff;
+    }
+`;
+export const CapitalsWeather = styled.div``;
