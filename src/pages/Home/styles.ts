d1093c17eca6b6aa676df0a8295ec8f05dde6144
Fabio Ramos <fabioalvesramos12@gmail.com>
2021-08-06 19:49:25 -0300

    changing home style

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     margin: 0 auto;
-    height: 100vh;
+    min-height: 100vh;
     width: 500px;
 
     display: flex;
