18e6435aa09cabf2667066d763a355c92a7dc03d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-30 23:18:37 -0400

    Change width

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -3,8 +3,8 @@ import SectionStyled from './styles';
 const Loading = () => (
   <SectionStyled>
     <img
-      width="150"
-      height="150"
+      width="100"
+      height="100"
       src="./svg/Spinner-1s-100px.svg"
       alt="Imagem de carregamento."
       aria-label="Imagem de carregamento."
