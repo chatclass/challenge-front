f620f9bb66b5814ae9702b6c362b2983cdf11c2d
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-10-01 13:38:45 -0400

    Loading options

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,15 +1,34 @@
+import { bool } from 'prop-types';
 import SectionStyled from './styles';
 
-const Loading = () => (
+const Loading = ({ orange }) => (
   <SectionStyled>
-    <img
-      width="100"
-      height="100"
-      src="./svg/Spinner-1s-100px.svg"
-      alt="Imagem de carregamento."
-      aria-label="Imagem de carregamento."
-    />
+    {orange ? (
+      <img
+        width="100"
+        height="100"
+        src="./svg/Spinner-1s-100px-orange.svg"
+        alt="Imagem de carregamento."
+        aria-label="Imagem de carregamento."
+      />
+    ) : (
+      <img
+        width="100"
+        height="100"
+        src="./svg/Spinner-1s-100px.svg"
+        alt="Imagem de carregamento."
+        aria-label="Imagem de carregamento."
+      />
+    )}
   </SectionStyled>
 );
+
+Loading.defaultProps = {
+  orange: false,
+};
+
+Loading.propTypes = {
+  orange: bool,
+};
 
 export default Loading;
