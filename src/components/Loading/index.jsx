5daa059d2f156ffd0904b0354d3ec31d25db8e73
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-30 21:21:59 -0400

    Add Loading

---
+++ src/components/Loading/index.jsx
@@ -0,0 +1,15 @@
+import SectionStyled from './styles';
+
+const Loading = () => (
+  <SectionStyled>
+    <img
+      width="150"
+      height="150"
+      src="./svg/Spinner-1s-100px.svg"
+      alt="Imagem de carregamento."
+      aria-label="Imagem de carregamento."
+    />
+  </SectionStyled>
+);
+
+export default Loading;
---
+++ src/components/Loading/styles/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const SectionStyled = styled.section`
+  width: 100px;
+  height: 100px;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default SectionStyled;
